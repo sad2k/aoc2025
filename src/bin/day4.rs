4d2009631e53c284f71a8b679572ab91e11f1caf
Sergey Dubakov <serge.dubakov@gmail.com>
2025-12-04 19:46:32 +0000

    Day 4 part 1

---
+++ src/bin/day4.rs
@@ -0,0 +1,67 @@
+use std::collections::HashSet;
+use std::fs;
+use std::str::Lines;
+
+#[derive(Debug)]
+struct Grid {
+    rows: usize,
+    cols: usize,
+    rolls: HashSet<(i32, i32)>,
+}
+
+fn parse(lines: Lines<'_>) -> Grid {
+    let mut r: usize = 0;
+    let mut c: usize = 0;
+    let mut rolls: HashSet<(i32,i32)> = HashSet::new();
+    for line in lines {
+        for (c, ch) in line.chars().enumerate() {
+            if ch == '@' {
+                rolls.insert((r as i32, c as i32));
+            }
+        }
+        r += 1;
+        c = line.len();
+    }
+    Grid {
+        rows: r,
+        cols: c,
+        rolls,
+    }
+}
+
+fn part1(grid: &Grid) -> usize {
+    let mut res = 0;
+    for (r, c) in &grid.rolls {
+        if is_accessible(*r, *c, grid) {
+            res += 1;
+        }
+    }
+    res
+}
+
+fn is_accessible(r: i32, c: i32, grid: &Grid) -> bool {
+    let candidates: [(i32, i32); 8] = [
+        (r - 1, c - 1),
+        (r - 1, c),
+        (r - 1, c + 1),
+        (r, c - 1),
+        (r, c + 1),
+        (r + 1, c - 1),
+        (r + 1, c),
+        (r + 1, c + 1),
+    ];
+    candidates
+        .iter()
+        .filter(|(r, c)| grid.rolls.contains(&(*r, *c)))
+        .count()
+        < 4
+}
+
+fn main() {
+    let contents = fs::read_to_string("inputs/day4.txt").unwrap();
+    let lines = contents.lines();
+    let parsed = parse(lines);
+
+    // part 1
+    println!("{}", part1(&parsed));
+}
