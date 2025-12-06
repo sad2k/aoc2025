e88cdbb36c9d15b81747f0c6cbc1368d94707469
Sergey Dubakov <serge.dubakov@gmail.com>
2025-12-06 17:42:13 +0000

    Day 6 part 2

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -24,6 +24,46 @@ fn parse_part1(lines: Lines<'_>) -> Vec<Problem> {
     res
 }
 
+fn transpose_numbers(char_vecs: &Vec<&[char]>) -> Vec<u64> {
+    let mut res = Vec::new();
+    for char_idx in 0..char_vecs[0].len() {
+        let mut num = 0;
+        for char_vec in char_vecs.iter() {
+            let ch = char_vec[char_idx];
+            if ch != ' ' {
+                num = num * 10 + ch.to_digit(10).unwrap() as u64;
+            }
+        }
+        res.push(num);
+    }
+    res
+}
+
+fn parse_part2(lines: Lines<'_>) -> Vec<Problem> {
+    let char_vecs: Vec<_> = lines.map(|s| s.chars().collect::<Vec<_>>()).collect();
+    let op_indices = char_vecs[char_vecs.len() - 1]
+        .iter()
+        .enumerate()
+        .filter(|(i, ch)| **ch != ' ')
+        .collect::<Vec<_>>();
+    let mut res = Vec::new();
+    for i in 0..op_indices.len() {
+        let mut problem_char_vecs = Vec::new();
+        let from = op_indices[i].0;
+        for j in 0..char_vecs.len() - 1 {
+            let char_vec = &char_vecs[j];
+            let to = if i == op_indices.len() - 1 {
+                char_vec.len()
+            } else {
+                (op_indices[i + 1].0) - 1
+            };
+            problem_char_vecs.push(&char_vec[from..to]);
+        }
+        res.push((transpose_numbers(&problem_char_vecs), *op_indices[i].1));
+    }
+    res
+}
+
 fn solve(problem: &Problem) -> u64 {
     match problem.1 {
         '+' => problem.0.iter().sum(),
@@ -41,6 +81,10 @@ fn main() {
     let lines = contents.lines();
 
     // part 1
-    let problems = parse_part1(lines);
+    // let problems = parse_part1(lines);
+    // println!("{:?}", solve_all(&problems));
+
+    // part 1
+    let problems = parse_part2(lines);
     println!("{:?}", solve_all(&problems));
 }
