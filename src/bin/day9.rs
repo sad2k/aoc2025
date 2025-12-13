ff05ccd1890a4a3530812129e01086d1e281eb34
Sergey Dubakov <serge.dubakov@gmail.com>
2025-12-13 19:59:13 +0000

    Day 9 part 2 - build outer shell

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::iter::zip;
 use std::str::Lines;
 
 fn parse(lines: Lines) -> Vec<(i64, i64)> {
@@ -30,11 +31,130 @@ fn part1(coords: &[(i64, i64)]) -> i64 {
     max_area
 }
 
+#[derive(Debug)]
+enum CornerType {
+    TopRight,
+    TopLeft,
+    BottomRight,
+    BottomLeft,
+}
+
+#[derive(Debug)]
+enum CornerDirection {
+    Clockwise,
+    CounterClockwise,
+}
+
+#[derive(Debug)]
+enum LineDirection {
+    Left,
+    Right,
+    Down,
+    Up,
+}
+
+fn line_direction(p0: (i64, i64), p1: (i64, i64)) -> LineDirection {
+    if p1.0 - p0.0 > 0 {
+        LineDirection::Right
+    } else if p1.0 - p0.0 < 0 {
+        LineDirection::Left
+    } else if p1.1 - p0.1 > 0 {
+        LineDirection::Down
+    } else {
+        LineDirection::Up
+    }
+}
+
+fn classify_corner(corner: &[(i64, i64)]) -> (CornerType, CornerDirection) {
+    let dir1 = line_direction(corner[0], corner[1]);
+    let dir2 = line_direction(corner[1], corner[2]);
+    match (&dir1, &dir2) {
+        (LineDirection::Right, LineDirection::Down) => {
+            (CornerType::TopRight, CornerDirection::Clockwise)
+        }
+        (LineDirection::Right, LineDirection::Up) => {
+            (CornerType::BottomRight, CornerDirection::CounterClockwise)
+        }
+        (LineDirection::Right, LineDirection::Right) => panic!("bad corner: {dir1:?} {dir2:?}"),
+        (LineDirection::Right, LineDirection::Left) => panic!("bad corner: {dir1:?} {dir2:?}"),
+        (LineDirection::Left, LineDirection::Down) => {
+            (CornerType::TopLeft, CornerDirection::CounterClockwise)
+        }
+        (LineDirection::Left, LineDirection::Up) => {
+            (CornerType::BottomLeft, CornerDirection::Clockwise)
+        }
+        (LineDirection::Left, LineDirection::Left) => panic!("bad corner: {dir1:?} {dir2:?}"),
+        (LineDirection::Left, LineDirection::Right) => panic!("bad corner: {dir1:?} {dir2:?}"),
+        (LineDirection::Up, LineDirection::Left) => {
+            (CornerType::TopRight, CornerDirection::CounterClockwise)
+        }
+        (LineDirection::Up, LineDirection::Right) => {
+            (CornerType::TopLeft, CornerDirection::Clockwise)
+        }
+        (LineDirection::Up, LineDirection::Up) => panic!("bad corner: {dir1:?} {dir2:?}"),
+        (LineDirection::Up, LineDirection::Down) => panic!("bad corner: {dir1:?} {dir2:?}"),
+        (LineDirection::Down, LineDirection::Left) => {
+            (CornerType::BottomRight, CornerDirection::Clockwise)
+        }
+        (LineDirection::Down, LineDirection::Right) => {
+            (CornerType::BottomLeft, CornerDirection::CounterClockwise)
+        }
+        (LineDirection::Down, LineDirection::Down) => panic!("bad corner: {dir1:?} {dir2:?}"),
+        (LineDirection::Down, LineDirection::Up) => panic!("bad corner: {dir1:?} {dir2:?}"),
+    }
+}
+
+fn build_outer_shell(coords: &[(i64, i64)]) -> Vec<(i64, i64)> {
+    let mut res = Vec::new();
+    let mut corner_coords = Vec::new();
+    for i in 0..coords.len() {
+        let c = coords[i];
+        corner_coords.clear();
+        if i == 0 {
+            corner_coords.push(coords[coords.len() - 1]);
+            corner_coords.push(coords[0]);
+            corner_coords.push(coords[1]);
+        } else if i == coords.len() - 1 {
+            corner_coords.push(coords[i - 1]);
+            corner_coords.push(coords[i]);
+            corner_coords.push(coords[0]);
+        } else {
+            corner_coords.push(coords[i - 1]);
+            corner_coords.push(coords[i]);
+            corner_coords.push(coords[i + 1]);
+        }
+        let (corner_type, corner_direction) = classify_corner(&corner_coords);
+        res.push(match (corner_type, corner_direction) {
+            // clockwise
+            (CornerType::TopRight, CornerDirection::Clockwise) => (c.0 + 1, c.1 - 1),
+            (CornerType::BottomRight, CornerDirection::Clockwise) => (c.0 + 1, c.1 + 1),
+            (CornerType::BottomLeft, CornerDirection::Clockwise) => (c.0 - 1, c.1 + 1),
+            (CornerType::TopLeft, CornerDirection::Clockwise) => (c.0 - 1, c.1 - 1),
+            // counterclockwise
+            (CornerType::TopRight, CornerDirection::CounterClockwise) => (c.0 - 1, c.1 + 1),
+            (CornerType::BottomRight, CornerDirection::CounterClockwise) => (c.0 - 1, c.1 - 1),
+            (CornerType::BottomLeft, CornerDirection::CounterClockwise) => (c.0 + 1, c.1 - 1),
+            (CornerType::TopLeft, CornerDirection::CounterClockwise) => (c.0 + 1, c.1 + 1),
+        });
+    }
+    res
+}
+
+fn part2(coords: &[(i64, i64)]) -> i64 {
+    let outer_shell = build_outer_shell(coords);
+    println!("{:?}", outer_shell);
+    0
+}
+
 fn main() {
     let contents = fs::read_to_string("inputs/day9.txt").unwrap();
     let lines = contents.lines();
     let parsed = parse(lines);
+    println!("{:?}", parsed);
 
     // part 1
-    println!("{}", part1(&parsed));
+    // println!("{}", part1(&parsed));
+
+    // part 2
+    println!("{}", part2(&parsed));
 }
